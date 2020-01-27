5741f76c56b4f7c5e1200ad1d35f3956049847d0
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-27 16:24:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  create_an_array = ["Geralt", "Dandelion", "Yennefer", "Roach"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+   arrays_create_an_array = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array.shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
