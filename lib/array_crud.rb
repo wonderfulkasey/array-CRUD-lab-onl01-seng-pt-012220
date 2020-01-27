0abbc03a51f599c7c66ff5c9ffbd6571a5b98d3c
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-27 16:28:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-   arrays_create_an_array = create_an_array.pop
+   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift("wow")
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
